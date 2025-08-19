c13bd278bb46e5143df0aa7f7fc33e3f1c3f3745
Kim Carey <kthomas06@gmail.com>
2025-08-19 14:18:15 -0400

    Solution

---
+++ test_contains_duplicate.py
@@ -0,0 +1,18 @@
+"""
+Given an array, return True if any value appears at least twice, False otherwise.
+"""
+
+def contains_duplicate(nums):
+    seen = set()
+
+    for num in nums:
+        if num in seen:
+            return True
+        seen.add(num)
+    return False
+
+
+print(contains_duplicate([1, 2, 3, 1]))
+print(contains_duplicate([1, 2, 3, 4]))
+print(contains_duplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))
+print(contains_duplicate([5, 7, 9, 11]))
